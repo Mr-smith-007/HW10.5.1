2cd631768c57603a9b9ae90f4db2709b42f66969
Repnikov.A <repnikov.a@florcom.ru>
2023-08-24 15:27:35 +0300

    Закончено задание 1

--- HW10.5.1/Program.cs
+++ HW10.5.1/Program.cs
@@ -8,7 +8,11 @@ namespace HW10._5._1
 
         public static void Main(string[] args)
         {
-
+            Summ summ = new Summ(Reader.Read(), Reader.Read());
+            if (summ.X == 0 || summ.Y == 0)
+                Console.WriteLine("Рассчитать сумму невозможно");
+            else
+                Console.WriteLine($"Результат сложения: {summ.Sum(summ.X, summ.Y)}");
 
         }
     }
